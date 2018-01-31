bcd2ff787e805f25268f6d7931c716b7ad2552ea
DESKTOP-A792KI4\user <nkc.yuki.saitou@gmail.com>
2018-01-31 11:41:37 +0900

    タイトル、ステージセレクトなどを簡易的に実装

---
+++ Assets/3D/Script_s/StageSelect/StageSelectFade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelectFade : MonoBehaviour {
+
+    [Header("違うシーンから遷移してきたとき")]
+    public GameObject fadeOut;
+
+    void Start()
+    {
+        fadeOut.SetActive(true);
+        StartCoroutine(WaitFadeOut());
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    /// <summary>
+    /// 遷移してきたときの調整用
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator WaitFadeOut()
+    {
+        yield return new WaitForSeconds(2.5f);
+        fadeOut.SetActive(false);
+    }
+
+}
