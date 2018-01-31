5f69baf34d33934c0b3ea6ffb4966fe4c60fa998
DESKTOP-A792KI4\user <nkc.yuki.saitou@gmail.com>
2018-01-31 13:44:28 +0900

    タイトルムービーの実装

---
+++ Assets/3D/Script_s/Title/TitleMovie.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleMovie : MonoBehaviour {
+
+    float time = 0;
+
+    [Header("次のシーンに遷移するとき")]
+    public GameObject fadeIN;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (time < 10)
+        {
+            time += 1f * Time.deltaTime;
+        }
+
+        if (time >= 10)
+        {
+            fadeIN.SetActive(true);
+            StartCoroutine(WaitFadeIn());
+        }
+    }
+
+    /// <summary>
+    /// フェードするときの調整用
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator WaitFadeIn()
+    {
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene("TitleMovie");
+    }
+}
---
+++ Assets/3D/Script_s/Title/TitleMovieScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMovieScene : MonoBehaviour {
+
+    public Animator anim;
+    AnimatorStateInfo stateInfo;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (Input.GetButtonDown("JoyStick_Action1"))
+        {
+            FadeManager.Instance.LoadScene("Title", 0.1f);
+        }
+
+        if (stateInfo.normalizedTime >= 1.0f)
+        {
+            FadeManager.Instance.LoadScene("Title", 0.1f);
+        }
+
+    }
+}
